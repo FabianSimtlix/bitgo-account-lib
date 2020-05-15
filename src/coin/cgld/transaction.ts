b0835cb618b1c46ca0da099337ce31e97a720d98
Fabian Esslinger <fesslinger@simtlix.com>
2020-05-15 14:08:27 -0300

    CeloTransaction handler

--- src/coin/cgld/transaction.ts
+++ src/coin/cgld/transaction.ts
@@ -1,34 +1,14 @@
 import { BaseCoin as CoinConfig } from '@bitgo/statics';
 import { Eth } from '../../index';
-import { InvalidTransactionError } from '../baseCoin/errors';
-import { KeyPair, Utils } from './';
+import { TxData } from '../eth/iface';
+import { CgldTransaction } from './types';
 
 export class Transaction extends Eth.Transaction {
-  private _encodedTransaction?: string;
-
   constructor(_coinConfig: Readonly<CoinConfig>) {
     super(_coinConfig);
   }
 
-  /**
-   * Sign the transaction with the provided key. It does not check if the signer is allowed to sign
-   * it or not.
-   *
-   * @param {KeyPair} keyPair The key to sign the transaction with
-   */
-  async sign(keyPair: KeyPair): Promise<void> {
-    // Check if there is a transaction to sign
-    if (!this._ethTransaction) {
-      throw new InvalidTransactionError('Empty transaction');
-    }
-    this._encodedTransaction = await Utils.sign(this._ethTransaction.toJson(), keyPair);
-  }
-
-  /** @inheritdoc */
-  toBroadcastFormat(): any {
-    if (!this._encodedTransaction) {
-      return super.toBroadcastFormat();
-    }
-    return this._encodedTransaction;
+  setTransactionData(txData: TxData): void {
+    this._ethTransaction = CgldTransaction.fromJson(txData);
   }
 }
